c4d6060345236b60d0adf028f909015b58643102
Saidumar Rahmonov <133119544+saidumarrahmonov6@users.noreply.github.com>
2024-02-26 15:09:02 +0500

    oxirgi yangilanish

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:contact/malumot.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'malumot.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: MyApp(),
-    debugShowCheckedModeBanner: false,
-  ));
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MaterialApp(home: MyApp(), debugShowCheckedModeBanner: false,));
 }
 
 class MyApp extends StatefulWidget {
@@ -16,6 +23,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  CollectionReference logins = FirebaseFirestore.instance.collection("logins");
+
   TextEditingController logintxt = TextEditingController();
   TextEditingController passwordtxt = TextEditingController();
 
@@ -25,34 +34,65 @@ class _MyAppState extends State<MyApp> {
       appBar: AppBar(
         title: Text("Contacts"),
       ),
-      body: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.all(10),
-            padding: EdgeInsets.only(left: 10),
-            decoration: BoxDecoration(),
-            child: TextField(
-              controller: logintxt,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(), label: Text("Login")),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            padding: EdgeInsets.only(left: 10),
-            decoration: BoxDecoration(),
-            child: TextField(
-              controller: passwordtxt,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(), label: Text("Password")),
-            ),
-          ),
-          MaterialButton(
-            onPressed: () {},
-            child: Text("Login"),
-          )
-        ],
-      ),
+      body: StreamBuilder(
+        stream: logins.snapshots(),
+          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot<Object?>> snapshot){
+            if(snapshot.hasData){
+              return ListView.builder(
+                itemCount: snapshot.data!.size,
+                itemBuilder: (BuildContext context , int index){
+                  DocumentSnapshot document = snapshot.data!.docs[index];
+                  return Column(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.all(10),
+                        padding: EdgeInsets.only(left: 10),
+                        decoration: BoxDecoration(),
+                        child: TextField(
+                          controller: logintxt,
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(), label: Text("Login")),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(10),
+                        padding: EdgeInsets.only(left: 10),
+                        decoration: BoxDecoration(),
+                        child: TextField(
+                          controller: passwordtxt,
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(), label: Text("Password")),
+                        ),
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.all(10),
+                        height: 40,
+                        child: MaterialButton(
+                          onPressed: () {
+                            check(logintxt.text, passwordtxt.text, document);
+                          },
+                          child: Text("Login"),
+                          color: Colors.green,
+                        ),
+                      )
+                    ],
+                  );
+                },
+              );
+            } else if(snapshot.connectionState == ConnectionState.waiting){
+              return Center(child: CircularProgressIndicator(),);
+            } else {
+             return Center(child: Text("Nomalum xatolik !"),);
+            };
+          }
+      )
     );
   }
+  check(String login , String password , document){
+    if(login == document['login'] && password == document['password']){
+      Navigator.push(context, MaterialPageRoute(builder: (_)=> MalumotApp()));
+    }
+  }
 }
+
---
+++ lib/malumot.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MalumotApp extends StatefulWidget {
+  const MalumotApp({super.key});
+
+  @override
+  State<MalumotApp> createState() => _MalumotAppState();
+}
+
+class _MalumotAppState extends State<MalumotApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Text("amdsasd"),
+    );
+  }
+}
